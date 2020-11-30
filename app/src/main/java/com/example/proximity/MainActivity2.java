549e131b1a4597b14eb3982a4a39a5f82dc38bf0
Ali <50777787+UNImakeup@users.noreply.github.com>
2020-11-30 20:05:19 +0100

    Har tilføjet rygbøjninger

--- app/src/main/java/com/example/proximity/MainActivity2.java
+++ app/src/main/java/com/example/proximity/MainActivity2.java
@@ -3,7 +3,6 @@ package com.example.proximity;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -12,11 +11,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor proximitySensor;
     private SensorEventListener proximitySensorListener;
@@ -25,39 +23,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        final TextView textview=(TextView) findViewById(R.id.textView);
+        final TextView textview=(TextView) findViewById(R.id.textView4);
 
         if(proximitySensor == null){
             Toast.makeText(this, "Proximity sensor not available !", Toast.LENGTH_LONG).show();
             finish();
         }
 
-
-
-
         proximitySensorListener = new SensorEventListener() {
             int reps = 0;
+            boolean rep;
+
+            //Fungerende med rygbøjninger.
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
+                float currentValue = sensorEvent.values[0];
+                System.out.println(currentValue); // Skal bare lige se hvad værdierne er
 
-                if (sensorEvent.values[0] < proximitySensor.getMaximumRange()){
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                    reps++;
-                    if(reps == 2){
-                        Intent a = new Intent(MainActivity.this, MainActivity2.class);
-                        startActivity(a);
-                    }
-                } else {
-                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
-                }
+
+                 if(currentValue == 0.0){
+                     rep = false;
+                 }
+                 if (currentValue == 5.0 && rep == false){
+                     reps++;
+                     rep = true;
+                 }
                 System.out.println(reps);
-                textview.setText(String.valueOf(reps));
+                 textview.setText(String.valueOf(reps - 1)); //-1, fordi den starter på 1 af en eller anden grund.
             }
-
-
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
@@ -72,4 +69,4 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         sensorManager.unregisterListener(proximitySensorListener);
     }
-}
+    }
